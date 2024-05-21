b14faf1213adfc1cded5c41940b0c0efed68db86
Sourav <ksourav662@gmail.com>
2024-05-21 23:22:37 +0530

    initial commit

---
+++ First-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Jsx from "./components/Jsx";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Jsx />
+    </>
+  );
+}
+export default App;
---
+++ First-react-app/src/components/Jsx.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jsx() {
+  const name = "Thor";
+  const age = 1000;
+  return (
+    <div>
+      <h1>
+        Hello my name is {name} and my age is {age}
+      </h1>
+    </div>
+  );
+}
+
+export default Jsx;
