50aefa3d2fab295271d3a1382d08eb710e91230a
Sourav <ksourav662@gmail.com>
2024-05-22 14:10:42 +0530

    Tic-Tac-Toe

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Board from "./components/Board";
+function App() {
+  return (
+    <div className="app-container">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
